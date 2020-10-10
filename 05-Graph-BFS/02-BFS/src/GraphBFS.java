0741174015767d45f0ec913966e002c32285ebb0
dengxin05 <dengxin05@baidu.com>
2020-10-10 19:44:23 +0800

    finish chapter5 and chapter6

---
+++ 05-Graph-BFS/02-BFS/src/GraphBFS.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class GraphBFS{
+    private Graph G;
+    private boolean[] visited;
+
+    private ArrayList<Integer> order = new ArrayList<>();
+
+    public GraphBFS(Graph G){
+        this.G = G;
+        visited = new boolean[G.V()];
+        for(int v=0; v<G.V(); v++)
+            if(!visited[v]){
+                bfs(v);
+            }
+    }
+
+    private void bfs(int s){
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(s);
+        while(!queue.isEmpty()){
+            int v = queue.remove();
+            order.add(v);
+
+            for(int w : G.adj(v)){
+                if(!visited[w]){
+                    queue.add(w);
+                    visited[w] = true;
+                }
+            }
+        }
+    }
+
+    public Iterable<Integer> order(){
+        return order;
+    }
+
+    public static void main(String[] args){
+        Graph g = new Graph("05-Graph-BFS/02-BFS/g.txt");
+        GraphBFS graphBFS = new GraphBFS(g);
+        System.out.println("BFS Order : " + graphBFS.order());
+    }
+}
